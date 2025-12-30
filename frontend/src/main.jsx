a45a99a47a90205f901b67e6abd90188489bcdf2
SathishNadar <sathishmacbook01@gmail.com>
2025-12-30 15:13:08 +0530

    removed strict mode for repetitive api calls.. (2api per call issue is fixed)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import 'material-icons/iconfont/material-icons.css'
 import App from './App.jsx'
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-  </StrictMode>,
+  // <StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  // </StrictMode>,
 )
