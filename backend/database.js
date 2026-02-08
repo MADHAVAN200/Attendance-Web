250969156df27c582afc2793c3ff13cd0aeb8803
Mano-Bharathii <manobharathi189@gmail.com>
2026-02-08 12:29:07 +0530

    Port added to database connection

--- backend/database.js
+++ backend/database.js
@@ -8,6 +8,7 @@ export const knexDB = knex({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
   },
   pool: { min: 0, max: 10 },
 });
