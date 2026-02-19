4442dee14787349cce8d3962a634ce32787e8fe9
Mano-Bharathii <manobharathi189@gmail.com>
2026-02-19 15:34:14 +0530

    Production Backend Build 1.5.2

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -121,7 +121,7 @@ const Sidebar = ({ isMobileMenuOpen, setIsMobileMenuOpen }) => {
                     </button>
 
                     <div className="text-[10px] text-center text-slate-400 dark:text-slate-600 font-mono pt-2">
-                        beta v1.5.1
+                        beta v1.5.2
                     </div>
                 </div>
             </aside>
