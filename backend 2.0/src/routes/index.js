5eab2735daf75ab7e282ee6483033298f3137201
JayaSurya08-dev <suryabigboss08@gmail.com>
2026-03-04 23:46:54 +0530

    refactor: Reports apis are now organized into routes,services and controllers

--- backend 2.0/src/routes/index.js
+++ backend 2.0/src/routes/index.js
@@ -4,6 +4,7 @@ import authRoutes from './auth/authRoutes.js';
 import holidayRoutes from './holidays/holidayRoutes.js';
 import notificationRoutes from './notifications/notificationRoutes.js';
 import leaveRoutes from './leaves/leaveRoutes.js';
+import reportsRoutes from './reports/reportsRoutes.js';
 
 const router = express.Router();
 
@@ -13,7 +14,9 @@ router.use('/auth', authRoutes);
 router.use('/holiday', holidayRoutes);
 router.use('/policies', adminRoutes); // Since adminRoutes now exports /shifts
 router.use('/notifications', notificationRoutes);
-router.use('/leaves', leaveRoutes); // Since adminRoutes now exports /leaves
+router.use('/leaves', leaveRoutes);
+router.use('/admin/reports', reportsRoutes);
+router.use('/attendance/reports', reportsRoutes);
 router.use('/', adminRoutes); // For /locations which hits root
 
 router.get('/health', (req, res) => {
